502d221c5756cb00683798e82fd1bd8bcaa50150
Malay Maity <maitymalay27747@gmail.com>
2024-11-14 14:45:30 +0530

    Update Graph_Breadth_First_Search.java
    Breadth first search

--- Graph_Breadth_First_Search.java
+++ Graph_Breadth_First_Search.java
@@ -31,22 +31,6 @@ public class Graph_Breadth_First_Search{
         graph[5].add(new Edge(5, 6, 1));
         graph[6].add(new Edge(6, 5, 1));
     }
-    // public static void bfs(ArrayList<Edge>[]graph){
-    //     Queue <Integer> q=new LinkedList<>();
-    //     boolean vis[]=new boolean[graph.length];
-    //     q.add(0);
-    //     while(q.isEmpty()){
-    //         int curr=q.remove();
-    //         if(!vis[curr]){
-    //             System.out.println(curr+" ");
-    //             vis[curr]=true;
-    //             for(int i=0;i<graph[curr].size();i++){
-    //                 Edge e=graph[curr].get(i);
-    //                 q.add(e.dest);
-    //             }
-    //         }
-    //     }
-    // }
     public static void bfs(ArrayList<Edge>graph[]){
         Queue<Integer> q=new LinkedList<>();
         boolean vis[]=new boolean[graph.length];
